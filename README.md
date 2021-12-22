d0ba81bb2bf2fa1763eda038a4eb28dfc9f65bea
MarukoMa <zhj19881101@163.com>
2021-12-22 22:34:29 +0800

    更新

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
 # ajax
-笔者手动封装ajax请求，主要功能包含支持 method（get/post）、delay（延迟时间）、abort（取消请求）附加要求支持jsonp的调用。
+笔者手动封装ajax请求，主要功能包含  
+* 支持 method（get/post/delete/put  
+*  支持设置delay（延迟请求时间）、abort（取消请求）、timeout（超时的取消请求）
+
+
+## options
+
+* type  可设置get post put delete四种类型,默认为get方式  
+* url   请求地址
+* data  页发送到服务器的数据,get请求中将附加在 URL后。
+* async 请求均为异步请求,默认为true
+* timeout  设置超时的取消请求,默认为ms,初始值为0
+* delay 设置延迟请求时间,,默认为ms,初始值为0
+
+
+### Example
+```javascript
+  ajax({
+      type:"POST",
+      url:"http://localhost:3000/users",
+      data:{name:'小新',idNumber:"321*****11108"},
+      async:true,
+      dataType: "json",
+      timeout:0,
+      delay:30,
+      success:function(data){
+        console.log(JSON.stringify(data))
+      },
+      error:function(data){
+         console.log('err !')
+      }
+  })
+
+```
+
--- common/ajax.js
+++ common/ajax.js
@@ -5,7 +5,7 @@ function creatXMLObject(){
      return xhr
 }
 //参数处理
-function formatData(data) {
+function formatParams(data) {
     let dataStr = '';
     for (let item in data) {
         dataStr += ( dataStr ? "&" : "") + item + '=' + data[item]
@@ -13,12 +13,24 @@ function formatData(data) {
     return dataStr
 }
 //请求处理
-function sendHandle(opts,params,xhr){
-    const {type,url,async} = opts
-    if(type == "get"){ url += "?" + param }
-    xhr.open(type,url,async)  //初始化 HTTP 请求参数
+function sendHandle(opts,xhr){
+    const {type,url,async,delay} = opts
+    let params = formatParams(opts.data)
+    let reqUrl = url
+    if(type == "get"){reqUrl += ('?' + params)}
+    xhr.open(type,reqUrl,async)  //初始化 HTTP 请求参数
     if(type == "post"){ xhr.setRequestHeader("Content-Type","application/x-www-form-urlencoded")}
-    xhr.send(type == "get" ? null : params) //发送请求
+   
+    //延时设置
+    let delayRun = delay > 0 ? true : false
+    if(delayRun){
+        setTimeout(function(){
+            xhr.send(type == "get" ? null : params) //发送请
+        },delay)
+    }else{
+        xhr.send(type == "get" ? null : params) //发送请求
+    }
+    
 }
 function ajax(options){
     const xhr = creatXMLObject();
@@ -27,15 +39,26 @@ function ajax(options){
         url:options.url || '',
         data:options.data || null,
         async:options.async || true,
+        timeout:options.timeout||0,
+        delay:options.delay || 0,
         success:options.success,
         error:options.error
     }
-    const params = formatData(opts.data)
-    sendHandle(opts,params,xhr)
+    sendHandle(opts,xhr)
+    //超时设置
+    let isTimeout = false; //超时标记   
+    const timer = setTimeout(function(){
+        isTimeout = true;
+         xhr.abort();  //取消请求
+    },opts.timeout);
     //监听
     xhr.onreadystatechange = function(){
+       //超时处理
+        if(isTimeout){return}
+        clearTimeout(timer)
+       //延迟请求
         if(xhr.readyState === 4){  //HTTP响应全部接收
-            if(xhr.status === 200){
+            if(xhr.status>=200 && xhr.status<=400){
                 let resText = JSON.parse(xhr.responseText)
                opts.success(resText)
             }else{
@@ -43,5 +66,4 @@ function ajax(options){
             }
         }
     }
-
 }
